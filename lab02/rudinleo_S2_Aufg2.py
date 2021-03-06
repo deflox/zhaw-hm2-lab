be39f001ae98b5dbf7aa5442f522b9d439b5f8f6
deflox <git@deflox.net>
2021-03-06 16:43:18 +0100

    Fix issues with task 02 of the lab 02.

--- lab02/rudinleo_S2_Aufg2.py
+++ lab02/rudinleo_S2_Aufg2.py
@@ -1,5 +1,9 @@
 import sympy as sp
 
+x01 = 1
+x02 = 2
+x03 = 3
+
 # aufgabe a
 x1, x2 = sp.symbols('x1 x2')
 f1 = 5*x1*x2
@@ -7,7 +11,14 @@ f2 = x1**2*x2**2+x1+x2
 
 F = sp.Matrix([f1,f2])
 X = sp.Matrix([x1,x2])
-print('Jacobi Matrix a) \n', F.jacobian(X))
+M = F.jacobian(X)
+print('Jacobi Matrix a) \n', M)
+
+F0 = F.subs([(x1,x01),(x2,x02)])
+M0 = M.subs([(x1,x01),(x2,x02)])
+x0minusx = sp.Matrix([x1-x01,x2-x02])
+print('Linearisierung für a): \n', F0.evalf() + (M0.evalf() * x0minusx))
+print()
 
 # aufgabe b
 x1, x2, x3 = sp.symbols('x1 x2 x3')
@@ -17,4 +28,10 @@ f3 = (1)/(x3**2+x1**2)+x2**2
 
 F = sp.Matrix([f1,f2,f3])
 X = sp.Matrix([x1,x2,x3])
-print('Jacobi Matrix b) \n', F.jacobian(X))
+M = F.jacobian(X)
+print('Jacobi Matrix b) \n', M)
+
+F0 = F.subs([(x1,x01),(x2,x02),(x3,x03)])
+M0 = M.subs([(x1,x01),(x2,x02),(x3,x03)])
+x0minusx = sp.Matrix([x1-x01,x2-x02,x3-x03])
+print('Linearisierung für b): \n', F0.evalf() + (M0.evalf() * x0minusx))
